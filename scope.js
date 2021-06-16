436786c02b4e06b4ccef20f8d90a93e9d8ba5560
shivamforgithub <55950922+shivamforgithub@users.noreply.github.com>
2021-06-16 12:01:10 +0530

    Add files via upload

---
+++ scope.js
@@ -0,0 +1,23 @@
+var x = 20 , y = 30;
+function bloackscope() {
+	var x = 10 
+	console.log("Value of var is " + x);
+
+{
+	var x = 5;
+	console.log("Value inside block is " + x);
+}
+	console.log("Value outside block is " + x);
+}
+function localscope(){
+	var x = 5;
+	console.log("Value of var is " + x);
+}
+function localvar(){
+	var x = 10 ;
+	console.log("Value of var is " + x);
+	localscope();                           // function is envoked fuction call within a fuction
+	console.log("Value of var outside is " + x); // variable X is overriden here on global variable
+	console.log("Value of var is " + y);
+
+}

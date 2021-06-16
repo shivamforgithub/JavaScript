58f643614be26c8be9e5be362fee01e17bc2414b
shivamforgithub <55950922+shivamforgithub@users.noreply.github.com>
2021-06-16 12:34:52 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,14 @@
+function length(){
+	var name = "Shivam Agrawal";
+	var len = name.length;
+	console.log(len);
+}
+
+
+function lengthstr(){
+	var name = "Shivam Agrawal";
+	var len = "Shivam Agrawal".length;
+	console.log(len);
+}
+
+

84e48171148050c2f889536f50077583e864a250
shivamforgithub <55950922+shivamforgithub@users.noreply.github.com>
2021-06-29 15:52:38 +0530

    Add files via upload

--- key_presence.js
+++ key_presence.js
@@ -22,29 +22,28 @@ const person = {
 
 
 
-const path = [];
+var path = " ";
 
-function traverse(obj , key){
+function traverse(obj , key , paths ){
+	var newpath = obj.name;
+	path = path + paths;
 		for (x in obj){
 			if(typeof(obj[x]) != "object" && typeof(obj[x]) != "function"){
 				if(key == obj[x]){
-					path.push(x);
-					console.log("Key Found");
-					return true;
+					path = path +"/"+ x;
+
+					console.log("Key Found")
+					return[key , path]
 				}
 				
 			}
 			else if (typeof(obj[x]) == "object"){
-				if(traverse(obj[x], key)){
-					path.push(x);
-				}
+				traverse(obj[x], key , path +'/'+ x);
+				
 			}
-		}
-		
+		}		
 	}
 
-/*function clearpath(path){
-	while(path.length != 0)
-		path.pop();
-}*/
 
+traverse(person,"Agrawal",' ');
+console.log(path);
